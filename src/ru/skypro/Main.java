0e269ed161611d8e8f5adc130c5f1de393a89bd0
Dmitriy <whittemak@gmail.com>
2022-11-26 09:06:56 +0300

    дз

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Author first = new Author("Александр", "Пушкин");
         Author second = new Author("Михаил", "Лермонтов");
-        Book firstBook = new Book("Сказки", " Александр Пушкин", 2010);
-        Book secondBook = new Book("Стихи", " Михаил Леромонтов", 1995);
+        Book firstBook = new Book("Сказки", first, 2010);
+        Book secondBook = new Book("Стихи", second, 1995);
         firstBook.setYearOfPublication(2020);
     }
     }
